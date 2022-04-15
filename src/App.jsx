d67a18c1bd3832df65f415493b2d2c4524a52b45
samhcarrasco <samuelcarrasco@Samuels-Air.fios-router.home>
2022-04-15 08:59:21 -0400

    added navbar component still need to create buttons for navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import Nav from './components/Nav.jsx';
 // import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import { CssBaseline } from '@mui/material';
 
@@ -6,6 +7,7 @@ function App() {
   
   return(
     <div>
+      <Nav/>
     <h1>hello world</h1>
     </div>
   );
