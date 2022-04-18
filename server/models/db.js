812158048b20a945bd66c4f1eb567a19b65bce57
Mark Charles Smith <markcharlessmith@gmail.com>
2022-04-18 10:38:01 -0400

    set up mongoose schema

---
+++ server/models/db.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const MONGO_URI =
+  'mongodb+srv://samhcarrasco:Welcome$5@cluster0.lbedd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'pokemonData',
+  })
+  .then(() => console.log('connected to MongoDb'))
+  .catch((err) => console.log(err));
+
+const Schema = mongoose.Schema;
+
+const pokemonSchema = new Schema({
+  id: String,
+  name: String,
+  height: String,
+  weight: String,
+  abilities: [String],
+  types: [String],
+  animated_sprites: String,
+  hp: Number,
+  attack: Number,
+  defense: Number,
+  special_attack: Number,
+  secial_defense: Number,
+  speed: Number,
+  flavor_text: String,
+});
+
+const Pokemon = mongoose.model('pokemon', pokemonSchema);
+module.exports = { Pokemon };
