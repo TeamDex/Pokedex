f4233b4327f718991553cd33f8c246430fd8d131
Mark Charles Smith <markcharlessmith@gmail.com>
2022-05-19 16:36:12 -0700

    added pseudocode

--- server/models/db.js
+++ server/models/db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+
 const MONGO_URI =
   'mongodb+srv://samhcarrasco:Welcome$5@cluster0.lbedd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose
   .connect(MONGO_URI, {
+    // these are options for the connect method to parse the URI
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    // sets name of the database that our collections are a part of
     dbName: 'pokemonData',
   })
   .then(() => console.log('connected to MongoDb'))
@@ -31,4 +34,6 @@ const pokemonSchema = new Schema({
 });
 
 const Pokemon = mongoose.model('pokemon', pokemonSchema);
+
+// exports all the models in an object to be used in the controller
 module.exports = { Pokemon };
